79f4c04ff4ccb47b16cf69df6ea5d8cb3468a88f
Kamil Milik <kamilmilik@gmail.com>
2018-03-31 13:19:53 +0200

    commit foreground in period ( not work in xiaomi) fix bug when user not login in some device

--- app/src/main/java/kamilmilik/licencjat_gps_kid/Utils/App.kt
+++ app/src/main/java/kamilmilik/licencjat_gps_kid/Utils/App.kt
@@ -5,17 +5,29 @@ import android.util.Log
 import kamilmilik.licencjat_gps_kid.DemoJobCreator
 import com.evernote.android.job.JobManager
 import kamilmilik.licencjat_gps_kid.DemoSyncJob
+import com.google.firebase.auth.FirebaseAuth
 
 
 /**
  * Created by kamil on 30.03.2018.
  */
 class App : Application(){
+    private val TAG = App::class.java.simpleName
     override fun onCreate() {
         Log.i("App", "App onCreate")
         super.onCreate()
-        JobManager.create(this).addJobCreator(DemoJobCreator())
-        DemoSyncJob.ScheduleJob.scheduleAdvancedJob()
+        //this is needed to some device to work correctly notifications
+        var mAuthListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
+            val user = firebaseAuth.currentUser
+            if (user != null) {
+                Log.i(TAG,"user log in")
+                JobManager.create(this).addJobCreator(DemoJobCreator())
+                DemoSyncJob.ScheduleJob.scheduleAdvancedJob()
+            }else{
+                Log.i(TAG,"user not log in")
+            }
+        }
+        FirebaseAuth.getInstance().addAuthStateListener(mAuthListener)
 
     }
 }
